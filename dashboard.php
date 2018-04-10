cb8fb0d072d32fa9eacca7e19c1d1eed8a85890b
Ronster2018 <kron.simmons@student.fairfield.edu>
2018-04-10 18:35:41 -0400

    Added .htaccess to more places

--- dashboard.php
+++ dashboard.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CHAHEG_login_Signup</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
