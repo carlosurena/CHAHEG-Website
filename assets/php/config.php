79689b66355ab64848a807ddee22991d7e12078c
John Crowley <john.crowley@student.fairfield.edu>
2018-04-10 14:54:58 -0400

    Local to GoDaddyLocal
    Changed database to database for the go daddy website.

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "kron.simmons";
-$dbPassword = "";
-$dbName = "chaheg";
+$dbServername = "127.0.0.1";
+$dbUsername = "db_team1_agent";
+$dbPassword = "NF1RGUq{3P(+";
+$dbName = "db_team1"; // db_team1
 
 $conn = mysqli_connect($dbServername,$dbUsername, $dbPassword, $dbName );
 
