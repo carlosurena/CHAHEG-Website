2361af1567f0d95c2ea8bf04b118c202b41c4c29
Unknown <carlos.urena@student.fairfield.edu>
2018-05-01 19:50:08 -0400

    Report_form pg

--- report_form.php
+++ report_form.php
@@ -1,5 +1,7 @@
 <?php 
 session_start();
+include 'assets/php/config.php';
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -100,41 +102,36 @@ session_start();
                
                     <div class="form-group"><label class="control-label">Test </label><select required class="form-control" name="testname">
                             <option value="All">All</option>    
-                            <option value="Albertus Magnus College">Albertus Magnus College</option>
-                            <option value="Central Connecticut State University">Central Connecticut State University</option>
-                            <option value="Charter Oak State College">Charter Oak State College</option>
-                            <option value="Connecticut College">Connecticut College</option>
-                            <option value="Eastern Connecticut State University">Eastern Connecticut State University</option>
-                            <option value="Fairfield University">Fairfield University</option>
-                            <option value="Goodwin College">Goodwin College</option>
-                            <option value="Hartford Seminary">Hartford Seminary</option>
-                            <option value="Holy Apostles College and Seminary">Holy Apostles College and Seminary</option>
-                            <option value="Lincoln College of New England-Southington">Lincoln College of New England-Southington</option>
-                            <option value="Lyme Academy College of Fine Arts">Lyme Academy College of Fine Arts</option>
-                            <option value="Mitchell College">Mitchell College</option>
-                            <option value="Paier College of Art Inc">Paier College of Art Inc</option>
-                            <option value="Post University">Post University</option>
-                            <option value="Quinnipiac University">Quinnipiac University</option>
-                            <option value="Rensselaer Hartford Graduate Center Inc">Rensselaer Hartford Graduate Center Inc</option>
-                            <option value="Sacred Heart University">Sacred Heart University</option>
-                            <option value="Southern Connecticut State University">Southern Connecticut State University</option>
-                            <option value="St Vincent's College">St Vincent's College</option>
-                            <option value="Trinity College">Trinity College</option>
-                            <option value="United States Coast Guard Academy">United States Coast Guard Academy</option>
-                            <option value="University of Bridgeport">University of Bridgeport</option>
-                            <option value="University of Connecticut">University of Connecticut</option>
-                            <option value="University of Connecticut-Avery Point">University of Connecticut-Avery Point</option>
-                            <option value="University of Connecticut-Stamford">University of Connecticut-Stamford</option>
-                            <option value="University of Connecticut-Tri-Campus">University of Connecticut-Tri-Campus</option>
-                            <option value="University of Hartford">University of Hartford</option>
-                            <option value="University of New Haven">University of New Haven</option>
-                            <option value="University of Phoenix-Connecticut">University of Phoenix-Connecticut</option>
-                            <option value="University of Saint Joseph">University of Saint Joseph</option>
-                            <option value="Wesleyan University">Wesleyan University</option>
-                            <option value="Western Connecticut State University">Western Connecticut State University</option>
-                            <option value="Yale University">Yale University</option>
-                            <option value="Yale-New Haven Hospital Dietetic Internship">Yale-New Haven Hospital Dietetic Internship</option>
-                                
+                            <?php 
+                            $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+                            if (!$conn)
+                            {
+                               die("Connection failed: " . mysqli_connect_error());
+                            }
+                                $sql = "SELECT TestName, TestID FROM TestMaterials GROUP BY TestName ";
+                                $result = mysqli_query($conn, $sql);
+                                $resultCheck = mysqli_num_rows($result);
+                                $testnamesArray = array();
+                                $testIDArray = array();
+                                //echo "<option value='test1'>". $resultCheck; 
+                                $x=0;
+                                if (mysqli_num_rows($result) > 0)
+                                   {
+                                       while($row = mysqli_fetch_assoc($result))
+                                       {
+                                          $testnamesArray[$x] = $row["TestName"];
+                                          $testIDArray[$x] = $row["TestID"];
+                                           $x++;
+                                       }
+                                   }
+                                   //echo "</option>";
+                                $counter = 0;
+                                while($counter < $resultCheck)
+                                {
+                                    echo "<option value=\"".$testIDArray[$counter] ."\">". $testnamesArray[$counter]."</option>";
+                                    $counter++;
+                                }
+                            ?>
                         </select></div>
                         <button class="btn btn-success btn-block" type="submit">Search </button>
 
